a4703d694d5bb475d126f75c9bc43dd19cd871d1
saisowmya <saisowmya164@gmail.com>
2026-08-14 18:11:24 +0530

    Collections

---
+++ src/Collections/Arrayssss.java
@@ -0,0 +1,29 @@
+//collections are objects, to convert object to  datatypes we use wrapper classes
+package Collections;
+
+import java.util.ArrayList;
+
+public class Arrayssss {
+
+	public static void main(String[] args) {
+		
+		ArrayList al=new ArrayList();
+		
+		for(int i=1;i<=10;i++) {
+			if(i%2!=0) {
+				al.add(i);
+			}
+			
+			
+		}
+		System.out.println(al);
+		
+		//for each loop
+		/*Object od=(Integer) i;
+		foreach(Object i:al){*/
+			
+			
+		}
+	}
+
+}
